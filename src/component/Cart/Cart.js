0d83109932d56dc19681eb92cb9744eee02dca09
ankit-dutta <ankitdutta965@gmail.com>
2022-12-04 02:54:44 +0530

    add btn added

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,16 +1,86 @@
-import React,{useContext} from 'react';
+import React,{useContext,useState} from 'react';
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 import CartContext from '../../store/CartContext';
 
 
 const Cart = (props) =>{
+    const [quantity , setQuantity] = useState(1)
 
     const cartctx = useContext(CartContext)
-    let sum = 0
+    // let updateItem = 0
+    const existingItems = [...cartctx.items];
+
+    let amt = 0;
+
+    cartctx.items.forEach(item =>{
+        amt += Number((item.price)*(item.quantity));
+    })
+   
+    const addmealqnty = (item) =>{
+       
+    //   console.log('item add', item)
+    //   console.log(item.quantity)
+      const itemIdx = existingItems.findIndex((i) => i.id === item.id);
+      console.log('item id',existingItems[itemIdx].quantity)
+      const updateitem = existingItems[itemIdx].quantity++;
+      setQuantity(updateitem);
+      console.log('updated',updateitem)
+
+
+    }
+   
+    const removemealqnty = (item) =>{
+
+        const itemIdx = existingItems.findIndex((i)=>i.id===item.id);
+        console.log(itemIdx , 'itemidx')
+
+        if(item.quantity < 0){
+            return;
+        }
+
+        if(item.quantity > 0){
+            const updateitem = existingItems[itemIdx].quantity--;
+            setQuantity(updateitem);
+        }
+
+       
+        if(existingItems[itemIdx].quantity === 0){
+            cartctx.removeItem(item);
+        }
+      }
+
+    let totalAmount = 0
     const cartItem =( <ul className={classes['cart-items']}>{
+        
    cartctx.items.map((item)=>(
-    <li className={classes.upperlist}>Name: {item.name} || Price: {item.price} || Quantity: {item.quantity}</li>
+   
+    <li className={classes.upperlist} key={item.id} id = {item.id}>
+      
+          <div>
+            <h2>{item.name}</h2>
+          </div>
+
+        <div className={classes.itemlistcontainer}>
+         <div>
+            <span>
+                {`$${item.price}`}
+            </span>
+            <span> {`x${item.quantity}`}</span>
+         </div>
+
+         <div>
+           <button className={classes.addbtn} onClick={() => addmealqnty(item)} >+</button> &nbsp;
+           <button className={classes.removebtn} onClick={() => removemealqnty(item)} >-</button>
+         </div>
+       </div>
+       <hr></hr>
+
+
+        {/* Name: {item.name} || Price: {item.price} || Quantity: {item.quantity} 
+        <button>+</button>
+        <button>-</button> */}
+        </li>
     
     ))}
     </ul>
@@ -18,8 +88,8 @@ const Cart = (props) =>{
 
     //Adding total amount of items
     cartctx.items.map((item)=>{
-        sum = sum + item.price
-        console.log(sum)
+        totalAmount = totalAmount + item.price
+        console.log(totalAmount)
     })
 
 
@@ -29,7 +99,7 @@ const Cart = (props) =>{
 
         <div className={classes.total}>
             <span>Total Amount</span>
-            <span>{sum.toFixed(2)}</span>
+            <span>{amt.toFixed(2)}</span>
         </div>
 
         <div className={classes.actions}>
